e4513edc5b89df863567164d2f2c46bd401351e6
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2025-09-12 12:45:50 +0200

    feat: mise à jour du script de pré-construction pour récupérer la version du firmware

--- before_script.py
+++ before_script.py
@@ -1,39 +1,18 @@
-# Import("env")
-# import requests
-# from pathlib import Path
+import os
+import hashlib
+import requests
+from SCons.Script import Import
 
-# def pre_build_action(source, target, env):
-#     # Adresse de ton serveur Hono
-#     server_url = "http://178.16.130.164:3025/firmware/next-id"
-#     print("------------------------------------")
-#     print("Fetching next firmware version from server...")
-#     print("------------------------------------")
-#     try:
-#         # Récupère le next_id depuis l'API
-#         response = requests.get(server_url)
-#         response.raise_for_status()
-#         data = response.json()
-#         next_id = data.get("next_id")
+Import("env")
 
-#         if not next_id:
-#             print("Erreur : next_id non fourni par le serveur.")
-#             env.Exit(1)
+# -------------------
+# Pré-build : récupérer la version
+# -------------------
+version_url = "http://localhost:3000/prout-o-metre/firmwares/new-build-version"
+resp = requests.get(version_url)
+resp.raise_for_status()
+version = resp.json()["version"]
+print(f"[INFO] Version récupérée : {version}")
 
-#         print(f"Next firmware version from server: {next_id}")
-
-#         # Génère version.h
-#         version_h_path = Path("include/version.h")
-#         version_h_path.parent.mkdir(parents=True, exist_ok=True)
-#         with open(version_h_path, "w") as f:
-#             f.write("#ifndef VERSION_H\n#define VERSION_H\n\n")
-#             f.write(f"#define FIRMWARE_VERSION {next_id}\n")
-#             f.write("#endif // VERSION_H\n")
-
-#         print(f"Generated {version_h_path}")
-
-#     except requests.RequestException as e:
-#         print(f"Erreur lors de la récupération de next_id : {e}")
-#         env.Exit(1)
-
-# env.AddPreAction("$PROGPATH", pre_build_action)
-
+# Injecter dans le code
+env.Append(CPPDEFINES=[("FW_VERSION", f'"{version}"')])
